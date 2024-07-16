4efe1b9c688e28d38fc178e58bdbea8076547312
Abhishek Kumar <127426993+Abhishek-IITKGP@users.noreply.github.com>
2024-07-16 06:25:11 +0000

    Datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"  //treat all JS code as newer version
+
+//alert(3 + 3); we are using nodeJS not browser
+
+console.log(3+3); console.log('abhishek'); //always prefer to start from next line to improve code readability
+
+let name = 'abhishek'; //string
+let age = 18; //number
+let isLoggedIn = true; //boolean
+
+
+//THESE ARE PREMITIVE DATATYPES
+// number => range(2 to 2^52)
+// bigint => when the number is too large
+// boolean => true or false
+// string => ''
+// null => its a stand alone value, its a representation of empty value
+// undefined => when the value is not assigned
+// symbol => uniqueness 
+
+
+console.log(typeof(name)) //typeof to get the data type of the variable.
+
+console.log(typeof null); //type of null is object.
+
+console.log(typeof undefined); //type fo undefined is undefined.
