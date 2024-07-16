af20ef6f2a6c3ad5f4554a4d1c3f07e3599891df
Abhishek Kumar <127426993+Abhishek-IITKGP@users.noreply.github.com>
2024-07-16 11:40:41 +0000

    memory done

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,11 @@
+//Primitive datatypes => stored in Stack memory.
+//Non-premitive dattype => Stored in Head memory.
+
+//To manage memory in any programming language a garbadge collector is there that automatically allocated and dellocate memory.
+
+//*************STACK**************//
+//copy of the refernce is allocated to the variable
+//No change in the actual variable data
+
+//*************HEAP***********//
+//Actual reference is allocated to the variable
