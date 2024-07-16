5193eb278bccc21928f78f6d32747d2d862664e5
Abhishek Kumar <127426993+Abhishek-IITKGP@users.noreply.github.com>
2024-07-16 11:25:15 +0000

    datatypes summary

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -24,3 +24,37 @@ console.log(typeof(name)) //typeof to get the data type of the variable.
 console.log(typeof null); //type of null is object.
 
 console.log(typeof undefined); //type fo undefined is undefined.
+
+//Primitve datatypes: They all are callby values. That means when the data is allocated they are used by copying from the reference and not the whole refernce is used
+
+//total  7 categories: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+let id = Symbol('123');
+let myId = Symbol('123');
+
+console.log(id == myId);
+
+console.log(typeof id);
+
+//Reference type datatype or Non-primitive: The reference of data can be allocated.
+//Arrays, Objects and Functions
+
+//JS is a dynamic typping language. No need to define the datatype of the variable.
+
+//Array
+const heros = ['shaktimaan', 'naagraj', 'doga'];
+
+//Object
+let myObject = {
+    name: 'abhishek',
+    age: '23'
+}
+
+//Function
+const myFunction = function(){  //we can have our function in a varible also
+    console.log('Hello world!');
+}
+
+console.log(typeof myFunction); //return type will be function (Object function).
+console.log(typeof heros); //here also the return type is object.
+
