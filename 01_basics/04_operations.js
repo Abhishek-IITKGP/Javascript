b95e321a33270600dd8a3c0b14726c49e6906c06
Abhishek Kumar <127426993+Abhishek-IITKGP@users.noreply.github.com>
2024-07-16 11:01:53 +0000

    comparison done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,41 @@
+let value = -3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2 + 2);
+
+console.log(2**3);
+console.log(8%2);
+
+let str1 = 'abhishek'
+let str2 = 'kumar'
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + '2');
+console.log('1' + 2);
+console.log('1' + '2');
+
+console.log('1'+2+2);
+
+console.log(1+2+'2');
+
+console.log(+true);
+console.log(+'');
+
+let counter = 1;
+counter++; //postfix increment it returns the value before incrementing.
+console.log(counter);
+
+++counter; //prefix increment it returns the value after incrementing.
+console.log(counter);
+
+let x = 3;
+let y = x++;
+console.log('x:'+x,'y:+'y)
+
+
+let a = 5;
+let b = ++a;
+console.log('a'+a, 'b'+b);
---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,22 @@
+console.log(2 > 1);
+
+console.log('2'>1); //JS automatically converted datatype of '2' to 2.
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(0 != null);
+
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+//strict check '==='
+
+console.log('2' == 2); //check => datatype converted.
+
+console.log('2' === 2);//strict check => datatype is not converted.
