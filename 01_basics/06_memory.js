1552a5b12f0f8f18cc501e0a9962f80dd1a844d0
Abhishek Kumar <127426993+Abhishek-IITKGP@users.noreply.github.com>
2024-07-17 05:24:58 +0000

    memory updated

--- 01_basics/06_memory.js
+++ 01_basics/06_memory.js
@@ -1,5 +1,8 @@
+//JS uses automatic memory management where it uses *****garbage collection**** method to allocate and deallocate memory.
+//when the variable or object are in no use they become eligible for garbage collection for reclaiming the memory space.
+
 //Primitive datatypes => stored in Stack memory.
-//Non-premitive dattype => Stored in Head memory.
+//Non-premitive dattype => Stored in Heap memory.
 
 //To manage memory in any programming language a garbadge collector is there that automatically allocated and dellocate memory.
 
@@ -7,5 +10,6 @@
 //copy of the refernce is allocated to the variable
 //No change in the actual variable data
 
-//*************HEAP***********//
+//**************HEAP***************//
 //Actual reference is allocated to the variable
+//memory is allocated for the object and reference to the data.
