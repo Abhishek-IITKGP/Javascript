97b4dc15f9d491f004d12d6ce029ab545b9a09c2
Abhishek Kumar <127426993+Abhishek-IITKGP@users.noreply.github.com>
2024-07-17 07:29:11 +0000

    date and time complete

---
+++ 01_basics/09_dates_and_times.js
@@ -0,0 +1,22 @@
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate); //object datatype
+
+const myCreatedDate = new Date(2024,8,27);
+
+console.log(myCreatedDate.toLocaleDateString());
+
+let IndiaDate = new Date('09-27-2024');
+console.log(IndiaDate.toDateString());
+
+//******TIMESTAMPS*******//
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+console.log(myDate.getTime()); //time stamp from created date
+
+console.log(myDate.getDay());
